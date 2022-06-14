8a4c041eeaf8ac60e1056909599e1c7b03ac075c
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-14 13:09:12 +0300

    [ADD] key repository

--- src/utils/schemas.py
+++ src/utils/schemas.py
@@ -1,25 +1,37 @@
 from typing import Optional, Dict
 from dataclasses import dataclass
 
-from src.utils.types import API_NAME
+from src.utils.types import API_NAME, API_KEY, API_SECRET_KEY
+
+
+# <<<=====================================>>> Client <<<=============================================================>>>
 
 
 @dataclass
 class BodyPublicRequest:
-    httpMethod: str  # Http method. External: GET, POST
-    urlPath: str  # Binance/ByBit Method url
-    payload: Optional[Dict] = None  # Payload
+    httpMethod: str                                 # Http method. External: GET, POST
+    urlPath: str                                    # Binance/ByBit Method url
+    payload: Optional[Dict] = None                  # Payload
 
 
 @dataclass
 class BodyPrivateRequest:
-    apiName: API_NAME  # Api name in db
-    httpMethod: str  # Http method. External: GET, POST
-    urlPath: str  # Binance/ByBit Method url
-    __payload: Optional[Dict] = None  # Payload
+    apiName: API_NAME                               # Api name in db
+    httpMethod: str                                 # Http method. External: GET, POST
+    urlPath: str                                    # Binance/ByBit Method url
+    __payload: Optional[Dict] = None                # Payload
 
     @property
     def payload(self) -> Dict:
         if self.__payload is None:
             return {}
         return self.__payload
+
+
+# <<<=====================================>>> Repository <<<=========================================================>>>
+
+
+class BodySetKey:
+    apiName: API_NAME                               # Api name in db
+    apiKey: API_KEY                                 # Api key
+    secretKey: API_SECRET_KEY                       # Api secrey key
--- src/utils/types.py
+++ src/utils/types.py
@@ -1,1 +1,3 @@
 API_NAME: str                   # API name in db
+API_KEY: str                    # API key
+API_SECRET_KEY: str             # Api secret key
