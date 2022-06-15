b7c57327241ca2d03f9bf1608afc3acb34590430
Mamedov Murad <mamedov_99b@mail.ru>
2022-06-15 11:21:45 +0300

    [FIX]

--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 from tortoise import fields, models
 
 
+# <<<==========================================>>> Base models <<<===================================================>>>
+
+
 class UserModel(models.Model):
     id = fields.BigIntField(pk=True, unique=True)
     username = fields.CharField(max_length=255, null=True, default=None)
@@ -18,13 +21,22 @@ class WalletModel(models.Model):
     user_id = fields.OneToOneField('models.UserModel', related_name='wallet', on_delete=fields.CASCADE)
 
 
-class AccountModel:
-    id = fields.IntField(pk=True, unique=True)
-    api_name = fields.CharField(max_length=255, unique=True)
+class AccountModel(models.Model):
+    api_name = fields.CharField(max_length=255, pk=True, unique=True)
     api_key = fields.CharField(max_length=255, unique=True)
     secret_key = fields.CharField(max_length=255, unique=True)
     user_id = fields.ForeignKeyField('models.UserModel', related_name="user_id", on_delete=fields.CASCADE)
 
 
-class OrderModel:
+# <<<==========================================>>> Order models <<<==================================================>>>
+
+
+class OrderModel(models.Model):
+    pass
+
+
+# <<<==========================================>>> Transaction models <<<============================================>>>
+
+
+class TransactionModel(models.Model):
     pass
